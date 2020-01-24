7cd2322dbbb7301f6e2639c1f6d407903c968dc9
Nandakumar <a_nanadakumar@ats-hr420-dm22.fc.ca>
2020-01-24 14:58:44 -0500

    files

---
+++ script/app.js
@@ -0,0 +1,15 @@
+(() => {
+	// set up the puzzle pieces and boards
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+
+function changeImageSet() {
+
+ debugger;
+//this.classList
+
+}
+
+
+puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+})();
